1d58945cc41fc2528a0bb3498b2d818177d91e12
Benjamin Tari <mohammadtari@gmail.com>
2026-04-14 17:01:04 -0400

    Add: verifying-an-alien-dictionary - submission-1

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        n = len(words)
+        char_i = {c: i for i, c in enumerate(order)}  # mapping: char -> rank
+
+        for i in range(1, n):
+            a = words[i-1]
+            b = words[i]
+            j, k = 0, 0
+
+            while j < len(a) and k < len(b):
+                if char_i[a[j]] > char_i[b[k]]:
+                    return False
+                elif char_i[a[j]] == char_i[b[k]]:
+                    j += 1
+                    k += 1
+                else:
+                    break
+
+            # If we compared all character in two consecutive words and they are the same,
+            # The shorter must appear before longer, otherwise it's not correctly ordered.
+            if j < len(a) and k == len(b):
+                return False
+
+        return True 
