428b295b1539165f852656c0747dd16f6be2c141
Benjamin Tari <mohammadtari@gmail.com>
2026-04-18 12:40:00 -0400

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.py
@@ -0,0 +1,38 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = {}
+
+        def dfs(i, canSell):
+            if i == n:
+                return 0
+
+            if (i, canSell) in dp:
+                return dp[(i, canSell)]
+
+           # sell
+            if canSell:
+                sell = dfs(i + 1, False) + prices[i]
+
+            # buy
+            buy = dfs(i + 1,  True) - prices[i]
+
+            # skip
+            skip = dfs(i + 1, canSell)
+
+            res = max(buy, sell, skip) if canSell else max(buy, skip)
+            dp[(i, canSell)] = res
+            return res
+        
+        return dfs(0, False)
+
+
+
+
+
+#                                        buy/sell/skip   
+#   i = 0                    -7                   0                   0
+#   i = 1             -8   -6  -7       -1   0    0          -1 0  0
+#   i = 2                                -6 4 -1
+
+                        
