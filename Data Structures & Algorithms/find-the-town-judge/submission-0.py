dbdf9c8f6f1ad863b91fe5de631aafa968744d01
Benjamin Tari <mohammadtari@gmail.com>
2026-04-24 11:52:13 -0400

    Add: find-the-town-judge - submission-0

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-0.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        adj = defaultdict(set)
+
+        for u, v in trust:
+            adj[u].add(v)
+
+        # Find a node with out-degree of zero 
+        cnt = 0
+        candidate = None
+        for i in range(1, n + 1):
+            if i not in adj:
+                candidate = i
+                cnt += 1
+            # There must be exactly one person as a judge
+            if cnt > 1:
+                return -1
+
+        if not candidate:
+            return -1
+
+        # Check if the out-degree of candidate is  n - 1
+        out_deg = 0
+        for node, neis in adj.items():
+            if node != candidate and candidate in neis:
+                out_deg += 1
+
+        return candidate if out_deg == n - 1 else -1
